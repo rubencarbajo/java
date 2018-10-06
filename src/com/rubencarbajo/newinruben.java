e04c4f483e67f462c2e95d724479b7723cece7ad
rubencarbajo <41683734+rubencarbajo@users.noreply.github.com>
2018-10-06 17:22:49 +0200

    Update newinruben.java

--- src/com/rubencarbajo/newinruben.java
+++ src/com/rubencarbajo/newinruben.java
@@ -3,6 +3,6 @@ package com.rubencarbajo;
 public class newinruben {
 
 	public String Ruben="This has beeen local updated in rube branch";
-	public String master="this is web updated by master";
+	public String master="this is web updated by master again";
 
 }
