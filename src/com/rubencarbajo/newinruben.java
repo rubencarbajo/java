5d4fb15bf4d331c50b754b43cbc3f4b22e4e0844
rubencarbajo <ruben.carbajo@gmail.com>
2018-10-06 17:23:48 +0200

    m

--- src/com/rubencarbajo/newinruben.java
+++ src/com/rubencarbajo/newinruben.java
@@ -2,7 +2,7 @@ package com.rubencarbajo;
 
 public class newinruben {
 
-	public String Ruben="This has beeen local updated in rube branch";
+	public String Ruben="This has beeen local updated in ruben branch";
 	public String master="this is web updated by master";
 
 }
